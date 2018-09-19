e0331824c29eecef0fb184f5cdf804975f04a860
Andy Law <andy.law@roslin.ed.ac.uk>
2018-09-19 17:25:38 +0100

    Initial tinkering

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -21,13 +21,16 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
+    //app.quit()
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function() {
+  createWindow();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
