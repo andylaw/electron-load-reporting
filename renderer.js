262249f5ba1825842f511ad5c9328cb537195733
Andy Law <andy.law@roslin.ed.ac.uk>
2018-09-20 17:27:50 +0100

    Initial data structure with loads included

--- renderer.js
+++ renderer.js
@@ -1,8 +1,26 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+ds = require('./app/js/datasource.js')
+
 function setColour () {
-    document.getElementById('statsPanel').style.color = 'red';
+    sp = document.getElementById('statsPanel');
+    sp.style.color = 'red';
 }
 
 document.getElementById('ping').addEventListener('click', setColour);
+
+
+function renderStatus(value) {
+    return "<p>" + value.node + "</p>";
+}
+
+
+let data = ds();
+let statsPanel = document.getElementById('statsPanel');
+
+statsPanel.innerHTML = "";
+
+data.forEach(function(value) {
+    statsPanel.innerHTML += renderStatus(value);
+});
