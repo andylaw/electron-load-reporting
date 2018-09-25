2d2aaae1199362e351645554e0f44d2306cde633
Andy Law <andy.law@roslin.ed.ac.uk>
2018-09-25 15:47:18 +0100

    Updated d3 to something more modern. Now have functioning code that will display the load as well as the allocated and total cores per node

--- renderer.js
+++ renderer.js
@@ -4,33 +4,48 @@
 ds = require('./app/js/datasource.js');
 d3 = require('d3');
 
-function setColour () {
-    sp = document.getElementById('statsPanel');
-    sp.style.color = 'red';
+/**
+ * Fills in the current status values for each node in our set of interest.
+ *
+ */
+function fill_in_status() {
+
+    // Get the data from some data source or another
+    let node_data = ds();
+
+    // Grab the Status Panel and all the divs that it currently contains.
+    // Join it with the data to return a list of elements that currently
+    // exist, matched up with their corresponding data point
+    let my_divs = d3.select("#statsPanel").selectAll("div.top")
+        .data(node_data);
+
+    // Throw away any divs that we may not require...
+    // (this should not ever happen, but hey, why not?)
+    my_divs.exit().remove();
+
+    // Now add on as many additional div elements that we need
+    // and merge them all with their data (existing and additional)
+    // In doing so, we slap a 'top' class on the top-level div so
+    // we only pull those out in the above select next time around
+    my_divs
+        .enter().append("div")
+        .attr("class", "top")
+        .merge(my_divs)
+        .text(function (d) {
+            return d.node;
+        })
+        .selectAll("div.scoreDiv")
+        .data(function (d) {
+            return [d];
+        })
+        .enter().append("div")
+        .attr("class", "scoreDiv")
+        .text(function (d) {
+            return [d.allocated, d.total, d.load].join(", ");
+        })
+    ;
 }
 
-document.getElementById('ping').addEventListener('click', setColour);
+document.getElementById('ping').addEventListener('click', fill_in_status);
 
-
-function renderStatus(value) {
-    return "<p>" + value.node + "</p>";
-}
-
-
-let node_data = ds();
-// let statsPanel = document.getElementById('statsPanel');
-//
-// statsPanel.innerHTML = "";
-//
-// data.forEach(function(value) {
-//     statsPanel.innerHTML += renderStatus(value);
-// });
-//
-//
-
-let my_divs = d3.select("#statsPanel").selectAll("div").data(node_data);
-
-my_divs.enter().append("div").attr("class", "statusDiv")
-    .text(function(d) {
-        return "<p>" + d.node + "</p>";
-    });
+fill_in_status();
