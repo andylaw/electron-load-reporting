819778e9057eaeae2ea584769caf99565f82e29e
Andy Law <andy.law@roslin.ed.ac.uk>
2018-10-01 16:39:06 +0100

    Initial work using simple Javascript to draw SVG. Possibly more understandable to real people than the D3 code?

--- renderer.js
+++ renderer.js
@@ -4,78 +4,67 @@
 ds = require('./app/js/datasource.js');
 d3 = require('d3');
 
-/**
- * Fills in the current status values for each node in our set of interest.
- *
- */
-function fill_in_status() {
+function do_svg_panel(svg_panel, load_data) {
 
-    /**
-     * Given a node type and a measure of the difference between expected
-     * and actual work effort, return a string that indicates the status in
-     * the form of a CSS class name
-     * @param type
-     * @param proportion
-     */
-    function get_status_class(type, proportionGap) {
-        let alert_class = "";
-        if ((type == 'BI') || (type == 'IC')) {
-            alert_class = "safe"
+    const name_column_start = 0;
+    const name_column_width = 250;
+    const type_column_start = name_column_start + name_column_width;
+    const type_column_width = 100;
+    const histo_column_start = type_column_start + type_column_width;
+    const histo_100_width = 400;
+    const panel_right_headroom = 100;
+    const canvas_width = name_column_width + histo_100_width + type_column_width + panel_right_headroom;
+    const padding_top = 4;
+    const padding_bottom = 4;
+    const inner_bar_height = 4;
+    const outer_bar_padding = 6;
+    const outer_bar_height = inner_bar_height + (outer_bar_padding * 2);
+    const row_height = outer_bar_height + padding_bottom + padding_top;
+    const font_size = outer_bar_height;
+    const font_padding = font_size + ((row_height - font_size) / 2);
+
+    let required_height = load_data.length * row_height;
+
+    let svg_canvas = svg_panel.append("svg:svg")
+        .attr("width", canvas_width)
+        .attr("height", required_height);
+
+    load_data.forEach(function(item, index) {
+        let y_offset = (index * row_height);
+
+        if (index % 2) {
+            svg_canvas.append("rect")
+                .attr("height", row_height)
+                .attr("width", canvas_width)
+                .attr("fill", "#EEEEEE")
+                .attr("x", 0)
+                .attr("y", y_offset);
         }
-        else if (proportionGap >= 0.1) {
-            alert_class = "veryBad";
-        }
-        else if (proportionGap >= 0.05) {
-            alert_class = "bad";
-        }
-        else {
-            alert_class = "good";
-        }
-        return alert_class;
-    }
+        svg_canvas.append("text")
+            .attr("x", 0)
+            .attr("y", y_offset + font_padding)
+            .attr("font-size", font_size)
+            .text(item.node);
 
-    // Get the data from some data source or another
-    let node_data = ds();
+        let used_proportion = item.allocated / item.total;
+        let load_proportion = item.load / item.allocated;
+        let load_absolute = item.load / item.total;
 
-    // Grab the Status Panel and all the divs that it currently contains.
-    // Join it with the data to return a list of elements that currently
-    // exist, matched up with their corresponding data point
-    let my_divs = d3.select("#statsPanel").selectAll("div.top")
-        .data(node_data);
+        svg_canvas.append("rect")
+            .attr("height", outer_bar_height)
+            .attr("width", histo_100_width * used_proportion)
+            .attr("fill", "grey")
+            .attr("x", histo_column_start)
+            .attr("y", y_offset + padding_top);
 
-    // Throw away any divs that we may not require...
-    // (this should not ever happen, but hey, why not?)
-    my_divs.exit().remove();
+        svg_canvas.append("rect")
+            .attr("height", inner_bar_height)
+            .attr("width", histo_100_width * load_absolute)
+            .attr("fill", "darkgrey")
+            .attr("x", histo_column_start)
+            .attr("y", y_offset + padding_top + outer_bar_padding);
 
-    // Now add on as many additional div elements that we need
-    // and merge them all with their data (existing and additional)
-    // In doing so, we slap a 'top' class on the top-level div so
-    // we only pull those out in the above select next time around
-    my_divs
-        .enter().append("div")
-        .attr("class", function(d) {
-            load_stat = Math.abs(1 - (d.load / d.total));
-            let alert_class = get_status_class(d.type, load_stat);
-            return "top " + alert_class;
-        })
-        .merge(my_divs)
-        .text(function (d) {
-            return d.node;
-        })
-        .selectAll("div.scoreDiv")
-        .data(function (d) {
-            return [d];
-        })
-        .enter().append("div")
-        .attr("class", "scoreDiv")
-        .text(function (d) {
-            load_stat = d.load / d.total;
-            difference = Math.abs(1 - load_stat);
-            return [d.allocated, d.total, d.load, load_stat, difference].join(", ");
-        })
-    ;
+    });
 }
 
-document.getElementById('ping').addEventListener('click', fill_in_status);
-
-fill_in_status();
+do_svg_panel(d3.select("#svgPanel"), ds());
